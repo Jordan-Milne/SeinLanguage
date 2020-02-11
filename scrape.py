dd159336b7d092f14559f8c891bb5f50d8680ba2
Jordan <jordan.milne2@gmail.com>
2020-02-11 16:11:12 -0500

    update

--- scrape.py
+++ scrape.py
@@ -1,7 +1,5 @@
 from gazpacho import get, Soup
 import re
-import requests
-from bs4 import BeautifulSoup
 import pandas as pd
 from itertools import groupby
 
@@ -14,24 +12,20 @@ links = [i.attrs['href'] for i in refs]
 links = [i.replace(" ", "") for i in links]
 links
 
-def same_line(link):
-    list2 = []
-    for i in link[5:-5]:
-        if re.match("^[a-zA-Z]*\:", i):
-            list2.append(i)
-        else:
-            list2[-1] += i
-    return list2
 
-url = 'https://seinfeldscripts.com/' + str(links[9])
-html = get(url)
-soup = Soup(html)
-table = soup.find('div', {'id' : 'content'})
-script = table.find('p')
-scrip = [i.remove_tags() for i in script]
-lines = same_line(scrip)
-scri = [i.replace('\n','') for i in lines]
-spaces = [re.sub(' +', ' ', i) for i in scri]
-lines = same_line(spaces)
-bracks = [re.sub('\[.*?\]', '', i) for i in lines]
-bracks
+def scrape_script(episode):
+    url = 'https://seinfeldscripts.com/' + str(episode)
+    html = get(url)
+    soup = Soup(html)
+    table = soup.find('div', {'id' : 'content'})
+    script = table.find('p')
+    scrip = [i.remove_tags() for i in script]
+    lines = same_line(scrip)
+    scri = [i.replace('\n','') for i in lines]
+    spaces = [re.sub(' +', ' ', i) for i in scri]
+    lines = same_line(spaces)
+    bracks = [re.sub('\[.*?\]', '', i) for i in lines]
+    return bracks
+
+scripts = [scrape_script(i) for i in links]
+pd.DataFrame(scripts, columns = ['script'])
